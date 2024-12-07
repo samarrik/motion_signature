d98fa243aad753a7c346781f31e88b8678b74242
Ivan Samarskyi <samariva@fit.cvut.cz>
2024-12-07 10:06:54 +0100

    Insane batch and workers

--- test.py
+++ test.py
@@ -16,8 +16,8 @@ video_prediction = detector.detect(
     video_to_tensor(video_path), 
     data_type="tensor",
     face_detection_threshold=0.8,
-    num_workers=5,
-    batch_size=100,
+    num_workers=30,
+    batch_size=1000,
 )
 
 # End timing
