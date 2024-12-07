e1f54b37f863a2fb91779b22fb15a47a982e7082
Ivan Samarskyi <samariva@fit.cvut.cz>
2024-12-07 08:59:07 +0100

    Fixed the installation script

--- install.sh
+++ install.sh
@@ -26,24 +26,14 @@ else
     exit 1
 fi
 
+conda activate ms-env
+pip install --upgrade pip setuptools wheel
+
 # Installing the development version of py-feat
 cd ~/miniconda3/envs/ms-env/lib
 git clone https://github.com/cosanlab/feat.git
 cd feat
 pip install -e .
-
-
-conda activate ms-env
-
-# Create symbolic links to NVIDIA shared libraries (if applicable)
-if python -c "import tensorflow" &>/dev/null; then
-    pushd $(dirname $(python -c 'print(__import__("tensorflow").__file__)'))
-    ln -svf ../nvidia/*/lib/*.so* . || echo "NVIDIA libraries not linked. Verify CUDA installation."
-    popd
-else
-    echo "Error: TensorFlow not installed. Verify your environment.yml or install manually."
-    exit 1
-fi
 
 echo "Everything is ready. Activate the virtual environment and run main.py."
 echo "Run the following command to activate the virtual environment:"
